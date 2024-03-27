5c9f1b38a9fd2c56b6199465187f42ec6e672b20
MaCAT <138701551+maximizemaxwell@users.noreply.github.com>
2024-03-28 04:43:48 +0900

    Title: 별 찍는 것을 5번 반복하기, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240328/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+void Printstars() {
+    for (int i = 0; i < 10; i++){
+        cout << "*";
+       
+    }
+     cout << endl;
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    for (int i = 0; i < 5; i++){
+        Printstars();
+    }
+    return 0;
+}
