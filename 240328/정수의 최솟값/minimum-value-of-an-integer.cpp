7abef7e982678c827a0f6f2abccb5b1d13356f37
MaCAT <138701551+maximizemaxwell@users.noreply.github.com>
2024-03-28 05:08:21 +0900

    Title: 정수의 최솟값, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240328/정수의 최솟값/minimum-value-of-an-integer.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int Minimum(int a, int b, int c) {
+    int min;
+    if (a > b) {
+        if (b > c) min = c;
+        else min = b;}
+    if (b > a) {
+        if (a > c) min = c;
+        else min = a;
+    }
+    return min;
+}
+
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b, c;
+    cin >> a >> b >> c;
+    int result = Minimum(a, b, c);
+    cout << result;
+    return 0;
+}
