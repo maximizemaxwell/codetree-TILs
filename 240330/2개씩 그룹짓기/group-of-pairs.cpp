e97b5a9b3dbb5d134099e824f9e78c5d2a4fa5d2
MaCAT <138701551+maximizemaxwell@users.noreply.github.com>
2024-03-30 01:29:21 +0900

    Title: 2개씩 그룹짓기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240330/2개씩 그룹짓기/group-of-pairs.cpp
+++ 240330/2개씩 그룹짓기/group-of-pairs.cpp
@@ -8,12 +8,12 @@ int main(void) {
     cin >> N;
     int *arr = new int[2 * N];
     for (int i = 0; i <2*N; i++)  cin >> arr[i];
-    //최댓값이 가장 작아지도록 분할
+    
     sort(arr, arr + 2 * N);
 
     int max = arr[0] + arr[2*N-1];
     for(int i = 0; i < N; i++){
-        int group = arr[i] + arr[2 * N - 1];
+        int group = arr[i] + arr[2 * N - 1 - i];
         if (group > max) max = group;
     }
     cout << max;
