ecff3700438cb8edcc382f2c203d916724d6300c
MaCAT <138701551+maximizemaxwell@users.noreply.github.com>
2024-08-03 16:28:19 +0900

    Title: 최고의 33위치, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240803/최고의 33위치/best-place-of-33.c
+++ 240803/최고의 33위치/best-place-of-33.c
@@ -3,15 +3,28 @@
 int main() {
     // 여기에 코드를 작성해주세요.
     int n;
-    int arr[n+1][n+1];
-    int sum = 0;
     scanf("%d", &n);
-    for (int i = 0; i < n; i++) {
+    int arr[20][20];
+
+    for (int i = 0; i < n; i ++) {
         for (int j = 0; j < n; j++) {
             scanf("%d", &arr[i][j]);
-            sum += arr[i][j];
         }
     }
-    printf("%d", sum);
+    int sum = 0;
+    int max = 0;
+
+    for (int i = 0; i <= n-3; i ++) {
+        for (int j = 0; j<=n-3; j++) {
+            sum = 0;
+            for (int k = i; k < i+3; k++){
+                for (int h = j; h < j+3; h++) {
+                    sum += arr[k][h];
+                }
+            }
+            if (sum > max) max = sum;
+        }
+    }
+    printf("%d", max);
     return 0;
 }
